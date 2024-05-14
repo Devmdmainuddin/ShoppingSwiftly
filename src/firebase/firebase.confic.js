2dc7225404e03ee1ab7c6e3d963ef1cb04aeb6fc
devmdmainuddin <devmdmainuddin@gmail.com>
2024-05-14 23:35:55 +0600

    firebase issu

--- src/firebase/firebase.confic.js
+++ src/firebase/firebase.confic.js
@@ -4,12 +4,7 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfic = {
 
-  // apiKey: "AIzaSyAvobOXu4-83xXMvXbg16NrYJFVWwFlJmM",
-  // authDomain: "shopswiftly-1c832.firebaseapp.com",
-  // projectId: "shopswiftly-1c832",
-  // storageBucket:"shopswiftly-1c832.appspot.com",
-  // messagingSenderId:"93854030821",
-  // appId:"1:93854030821:web:6c39e50c701d711b0eed0c"
+ 
 
 
   apiKey: import.meta.env.VITE_apiKey,
