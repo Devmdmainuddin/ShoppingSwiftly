a5c07225d67d0f4e40ae1471cbcdb0b78000bdc9
devmdmainuddin <devmdmainuddin@gmail.com>
2024-05-15 00:10:58 +0600

    firebase issu

--- src/firebase/firebase.confic.js
+++ src/firebase/firebase.confic.js
@@ -4,18 +4,14 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfic = {
 
- 
-
-
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain,
+  projectId:import.meta.env.VITE_projectId,
+  storageBucket:import.meta.env.VITE_storageBucket,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId,
+  appId:import.meta.env.VITE_appId
      
     
-
 };
 
 // Initialize Firebase
